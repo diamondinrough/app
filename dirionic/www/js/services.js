24b9f90148c94dd07f476c6d42c2f0cf280282e5
bryanly200 <bryanly200@gmail.com>
2016-07-28 14:32:58 -0700

    Update services.js

--- dirionic/www/js/services.js
+++ dirionic/www/js/services.js
@@ -97,3 +97,24 @@ angular.module('starter.services', [])
         });
     }
 }]);
+
+.service("HelpListSvc", ["$http", "$rootScope", "$ionicLoading", function($http, $rootScope, $ionicLoading) {
+    this.loadResources = function() {
+        $http.get(site + "api/app/help/?format=json")
+        .success(function(data) {
+            $rootScope.$broadcast("helplist", data);
+        })
+        .error(function() {
+            $ionicLoading.hide();
+        });
+    }
+}])
+
+.service("HelpSvc", ["$http", "$rootScope", function($http, $rootScope) {
+    this.loadResource = function(id) {
+        $http.get(site + "api/app/help/" + id + "/?format=json")
+        .success(function(data) {
+            $rootScope.$broadcast("help", data);
+        });
+    }
+}])
