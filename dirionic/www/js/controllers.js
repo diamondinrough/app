94906d8d206f4f1f25856f3ff74358c5dfc1ed2b
bryanly200 <bryanly200@gmail.com>
2016-09-05 14:20:08 -0700

    Update controllers.js

--- dirionic/www/js/controllers.js
+++ dirionic/www/js/controllers.js
@@ -1211,12 +1211,14 @@ angular.module('starter.controllers', [])
     	
     	 $scope.taglist = [];
        $scope.video.tags.forEach(function(tag) {
-         $scope.taglist.push({
-            name: tag.name,
-            color: tag.color,
-            checked: true
-          });
-        });
+         if (tag.name == "Entertainment") {
+           $scope.taglist.push({
+              name: tag.name,
+              color: tag.color,
+              checked: true
+            });
+         }
+       });
     	
         $scope.video.comments = [];
         data.comments.forEach(function(comment) {
@@ -1357,7 +1359,7 @@ angular.module('starter.controllers', [])
     };
     
   	VideoSvc.loadVideo($stateParams.id, "video");
-  	$scope.taglist = [{"name":"Entertainment", "color":"#FF3BFF", "checked":true}]
+//  	$scope.taglist = [{"name":"Entertainment", "color":"#FF3BFF", "checked":true}]
     VideoListSvc.loadVideos($scope.taglist, null, null, "rvideo-list");
 
 })
